c3272db15db4ec491cb6d69109dc99ce10e8dadc
fayzah <ffowz110@hotmail.com>
2024-04-08 04:06:24 +0300

    Initial Commit

---
+++ Fianal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fianal
+//
+//  Created by Faizah Almalki on 29/09/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fianal/FianalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FianalApp.swift
+//  Fianal
+//
+//  Created by Faizah Almalki on 29/09/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct FianalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
